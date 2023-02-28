f0ab265de6fa333e7e85bcd2e5831a20b3b9015b
Olesia Cheremnykh <chereole@fel.cvut.cz>
2023-02-28 12:11:49 +0100

    connect sqlite

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 import express from 'express';
+import sqlite from 'sqlite3';
 
 const app = express();
+app.use(express.json);
+let database;
+
+database = new sqlite.Database('./databases/default.db', sqlite.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+    console.log('Database OK')
+});
+
 app.get('/', (req, res) => {
     res.send('SQL simulator')
 })
 
 app.listen(8080, (err) => {
-    if (err) {
-        return console.log(err);
-    }
+    if (err) return console.log(err);
     console.log('Server OK');
 })
+
+
+
+
+
+
